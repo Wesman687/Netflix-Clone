9a5b16358645186f5a641a1601baf3dcae40c9f6
Paul <wesman687@gmail.com>
2024-07-08 12:59:45 -0400

    netflix clone

---
+++ src/Firebase.js
@@ -0,0 +1,56 @@
+
+
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCV_wU01_5uDbCd9IO1P92uWG0Eg0sqfE4",
+    authDomain: "netflix-clone-8798c.firebaseapp.com",
+    projectId: "netflix-clone-8798c",
+    storageBucket: "netflix-clone-8798c.appspot.com",
+    messagingSenderId: "498536584284",
+    appId: "1:498536584284:web:4f2f3115c7cf8c73130299"
+  };
+
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+const signup = async (name, email, password)=> {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password)
+        const user = res.user
+        await addDoc(collection(db, "user"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        })
+    } catch(error){
+        console.log(error)
+        alert(error)
+    }
+
+
+}
+
+const login = async (email, password) => {
+    try{
+        await signInWithEmailAndPassword(auth, email, password)
+
+    } catch (error) {
+        console.log(error)
+        alert(error)
+
+    }
+    
+}
+
+const logout = ()=> {
+    signOut(auth)
+}
+
+export { auth, db, login, signup, logout}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search_icon.svg";
+import bell_icon from "../../assets/bell_icon.svg";
+import caret_icon from "../../assets/caret_icon.svg";
+import profile_img from "../../assets/profile_img.png";
+import { auth, logout } from "../../Firebase";
+import { Link } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+const Navbar = () => {
+  const [signState, setSignState] = useState("Sign In")
+  useEffect(() => {
+    onAuthStateChanged(auth, async(user)=> {
+      if (user) {
+        setSignState("Sign Out")
+      }
+      else {
+        setSignState("Sign In")
+      }
+    })
+    
+  }, [])
+
+  const navRef = useRef();
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY >= 80) {
+        navRef.current.classList.add("nav-dark");
+      } else {
+        navRef.current.classList.remove("nav-dark");
+      }
+    });
+  }, []);
+  return (
+    <div ref={navRef} className="navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>Tv Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My list</li>
+          <li>Browse Languages</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={search_icon} alt="" />
+        <p>Children</p>
+        <img src={bell_icon} alt="" />
+        <div className="navbar-profile">
+          <img src={profile_img} alt="" />
+          <img src={caret_icon} alt="" />
+          <div className="dropdown">
+            {signState === "Sign Up" ? (
+              <p
+                onClick={() => {
+                  logout();
+                }}
+              >
+                Sign Out of Netflix
+              </p>
+            ) : (
+              <Link to="/login">
+                <p className="link">Sign In To Netflix</p>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
